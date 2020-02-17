22edf0a71a37fde04a3c4e34f5d5f057b3004589
Alex Aucestovar <aucestovara@gmail.com>
2020-02-16 19:08:17 -0600

    Persist Redux Store

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -54,8 +54,14 @@ function RenderCampsite(props) {
    
     const {campsite} = props;
     const recognizeDrag = ({dx}) => (dx < -200) ? true: false;
+    const view = React.createRef();
+
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+            view.current.rubberBand(1000)
+            .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
+        },
         onPanResponderEnd: (e, gestureState) => {
             console.log('pan responder end', gestureState);
             if (recognizeDrag(gestureState)) {
@@ -87,6 +93,7 @@ function RenderCampsite(props) {
                 animation='fadeInDown' 
                 duration={2000} 
                 delay={1000}
+                ref={view}
                 {...panResponder.panHandlers}
                 >
                 <Card
